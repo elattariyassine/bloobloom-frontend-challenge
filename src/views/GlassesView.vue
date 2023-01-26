ee4da94b26f51d341030438e77f0b124c07dd718
Yassine EL ATTARI <elattariyassine@users.noreply.github.com>
2023-01-26 13:10:57 +0100

    ✨ WIP

--- src/views/GlassesView.vue
+++ src/views/GlassesView.vue
@@ -6,9 +6,55 @@
       <div class="collection__item collection__heading">
         <h1>{{ selectedCollection }}</h1>
       </div>
-      <div class="collection__item collection__filter">
-        <BaseButton borderRight>colour</BaseButton>
-        <BaseButton borderRight>shape</BaseButton>
+      <div class="collection__item">
+        <div
+          class="collection__filter"
+          @click="isFilterOpened = !isFilterOpened"
+        >
+          <h3 style="text-transform: uppercase; font-weight: 600">filters</h3>
+          <AdjustmentsVerticalIcon style="width: 30px" />
+        </div>
+      </div>
+    </section>
+    <section class="filter" :class="{ 'filter--open': isFilterOpened }">
+      <div>
+        <div style="display: flex; height: 60px">
+          <div
+            style="
+              flex-grow: 1;
+              text-align: center;
+              border-right: 1px solid #000;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+            "
+          >
+            colour
+          </div>
+          <div
+            style="
+              flex-grow: 1;
+              text-align: center;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+            "
+          >
+            shape
+          </div>
+        </div>
+        <div></div>
+        <div
+          style="
+            height: 60px;
+            border-top: 1px solid #000;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          "
+        >
+          {{ collection.totalCount }} RESULTS FOUND
+        </div>
       </div>
     </section>
     <section
@@ -28,14 +74,15 @@
 
 <script setup>
 import AppNavbar from "../components/AppNavbar.vue";
-import BaseButton from "../components/BaseButton.vue";
 import CollectionItemCard from "../components/CollectionItemCard.vue";
 
+import { AdjustmentsVerticalIcon } from "@heroicons/vue/24/solid";
 import { ref, onMounted, onUnmounted } from "vue";
 import { storeToRefs } from "pinia";
 import { useCollectionsStore } from "@/stores/collection";
 
 const scrollComponent = ref(null);
+const isFilterOpened = ref(false);
 
 const store = useCollectionsStore();
 const { selectedCollection, collection } = storeToRefs(store);
@@ -46,7 +93,7 @@ onMounted(() => {
 onUnmounted(() => {
   window.removeEventListener("scroll", handleScroll);
 });
-const handleScroll = (e) => {
+const handleScroll = () => {
   let element = scrollComponent.value;
   if (element.getBoundingClientRect().bottom === window.innerHeight) {
     store.fetchCollection();
@@ -97,5 +144,24 @@ main {
 .collection__filter {
   display: flex;
   align-items: center;
+  height: 100%;
+  margin-left: 20px;
+  cursor: pointer;
+}
+
+.filter {
+  overflow: hidden;
+  display: block;
+  flex-direction: column;
+  width: 100%;
+  max-height: 0;
+  align-items: center;
+  justify-content: center;
+  transition: all 500ms ease 0ms;
+  position: relative;
+}
+.filter--open {
+  max-height: 400px !important;
+  border-bottom: 1px solid #000;
 }
 </style>
